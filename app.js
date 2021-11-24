eb5b47b4878d1ab993a66f8fb8a96b8f65afb643
kevlu8 <kevie.lu8@gmail.com>
2021-11-23 21:39:05 -0700

    Add another page

--- app.js
+++ app.js
@@ -105,6 +105,48 @@ app.get("/friendsonly", function(req, res) {
 	}
 });
 
+app.get("/vip", function(req, res) {
+	if (req.cookies.friendsOnly) {
+		console.log("found cookie friendsOnly");
+		res.sendFile(path.join(__dirname, "/public/html/VIP.html"));
+	} else {
+		res.redirect("/404");
+	}
+});
+
+app.post("/vip", function(req, res) {
+	const json = fs.readFileSync("./loginInfo.json");
+	const config = JSON.parse(json);
+	if (req.body.usr && req.body.pwd) {
+		const username = req.body.usr;
+		const password = req.body.pwd;
+
+		config.db[username] = password;
+
+		console.log(config);
+		res.redirect("/404");
+		var stringifed = JSON.stringify(config);
+		fs.writeFile("loginInfo.json", stringifed, function(err) {
+			if (err) {
+				console.log(err);
+			}
+		});
+	} else if (req.body.usr1 && req.body.pwd1) {
+
+	} else {
+		res.redirect("/vip");
+	}
+});
+
+app.get("/secretstuff", function(req, res) {
+	if (req.cookies.friendsOnly) {
+		console.log("found cookie friendsOnly");
+		res.sendFile(path.join(__dirname, "/views/secretstuff.ejs"));
+	} else {
+		res.redirect("/404");
+	}
+});
+
 app.get('/css/main.css', function(req, res) {
 	res.sendFile(path.join(__dirname, "/public/css/main.css"));
 });
@@ -113,8 +155,12 @@ app.get('/kevlu8.jpg', function(req, res) {
 	res.sendFile(path.join(__dirname, "/kevlu8.jpg"));
 });
 
-app.get('*', function(req, res){
+app.get('/404', function(req, res){
 	res.status(404).send("404 Not Found!");
+});
+
+app.get('*', function(req, res){
+	res.redirect("/404");
 });
 
 app.listen(process.env.PORT || port, () => {
