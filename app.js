1c419fb636aa306732099fba9d69df4c7177c730
kevlu8 <kevie.lu8@gmail.com>
2022-01-03 16:20:33 -0700

    Redid the entire site design

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const app = express();
 const path = require("path");
 const fs = require("fs");
 const readline = require("readline");
-const passwordGenerator = require("./passwordGenerator");
 const cookieParser = require("cookie-parser");
 
 app.set(express.static("public"));
@@ -14,51 +13,8 @@ app.use(cookieParser());
 
 const port = 8080;
 
-let secret = [];
-let isLoggedIn = false;
-
 app.get('/', function(req, res) {
-	if (req.cookies.loggedIn) {
-		console.log("found cookie loggedIn");
-		isLoggedIn = true;
-		res.sendFile(path.join(__dirname, "/public/html/index.html"));
-	} else if (req.cookies.friendsOnly) {
-		res.sendFile(path.join(__dirname, "/public/html/index.html"));
-	} else {
-		console.log("no cookie found");
-		res.redirect("/login");
-	}
-});
-
-app.get('/login', function(req, res) {
-	if (req.cookies.loggedIn) {
-		isLoggedIn = true;
-		res.redirect("/");
-	} else {
-		res.sendFile(path.join(__dirname, "/public/html/login.html"));
-	}
-});
-
-app.post('/login', function(req, res) {
-	const json = fs.readFileSync("./passwords.json");
-	const config = JSON.parse(json);
-
-	if (req.body.password == config.passwords.mainPwd) {
-		res.cookie("loggedIn", "true", { maxAge: 694200000, httpOnly: false });
-		res.redirect("/");
-	} else if (req.body.password == config.passwords.friendsOnly) {
-		res.cookie("loggedIn", "true", { maxAge: 694200000, httpOnly: false });
-		res.cookie("friendsOnly", "true", { maxAge: 694200000, httpOnly: false });
-		res.redirect("/");
-	} else {
-		res.redirect("/login");
-	}
-});
-
-app.get("/logout", function(req, res) {
-	res.clearCookie("loggedIn");
-	res.clearCookie("friendsOnly");
-	res.redirect('/');
+	res.sendFile(path.join(__dirname, "/public/html/index.html"));
 });
 
 app.get("/contact", function(req, res) {
@@ -73,82 +29,32 @@ app.get("/quotes", function(req, res) {
 	res.sendFile(path.join(__dirname, "/public/html/quotes.html"));
 });
 
-app.get("/friendsonly", function(req, res) {
-	if (req.cookies.friendsOnly) {
-		console.log("found cookie friendsOnly");
-		res.sendFile(path.join(__dirname, "/public/html/friendsOnly.html"));
-	} else {
-		res.redirect("/404");
-	}
+app.get("/ai", function(req, res) {
+	res.sendFile(path.join(__dirname, "/public/html/ai.html"));
 });
 
-app.get("/vip", function(req, res) {
-	if (req.cookies.friendsOnly) {
-		console.log("found cookie friendsOnly");
-		res.sendFile(path.join(__dirname, "/public/html/VIP.html"));
-	} else {
-		res.redirect("/404");
-	}
+app.get("/ai/ML-Tagger", function(req, res) {
+	res.sendFile(path.join(__dirname, "/public/html/ai/ML-Tagger.html"));
 });
 
-app.post("/vip", function(req, res) {
-	const json = fs.readFileSync("./loginInfo.json");
-	const config = JSON.parse(json);
-	if (req.body.usr && req.body.pwd) {
-		const username = req.body.usr;
-		const password = req.body.pwd;
-
-		config.db[username] = password;
-
-		console.log(config);
-		res.redirect("/404");
-		var stringifed = JSON.stringify(config);
-		fs.writeFile("loginInfo.json", stringifed, function(err) {
-			if (err) {
-				console.log(err);
-			}
-		});
-	} else if (req.body.usr1 && req.body.pwd1) {
-
-	} else {
-		res.redirect("/vip");
-	}
+app.get("/ai/ML-Tagger/nature", function(req, res) {
+	res.sendFile(path.join(__dirname, "/public/html/ai/ML-Tagger/nature.html"));
 });
 
-app.get("/secretstuff", function(req, res) {
-	if (req.cookies.friendsOnly) {
-		console.log("found cookie friendsOnly");
-		res.render(path.join(__dirname, "/views/secretstuff.ejs"));
-	} else {
-		res.redirect("/404");
-	}
+app.get("/ai/ML-Tagger/people", function(req, res) {
+	res.sendFile(path.join(__dirname, "/public/html/ai/ML-Tagger/people.html"));
 });
 
-app.post("/secretstuff", function(req, res) {
-	const sub = req.body.sub;
-	const time = req.body.time;
-	const sort = req.body.sort;
+app.get("/ai/ML-Tagger/food", function(req, res) {
+	res.sendFile(path.join(__dirname, "/public/html/ai/ML-Tagger/food.html"));
+});
 
-	const spawn = require("child_process").spawn;
-	const pythonProcess = spawn('python',["/home/kevlu8/kevlu8.dev/getReddit.py", sub, 10000, sort, time]);
-	
-	pythonProcess.stdout.on('Done', function(Done) {});
+app.get("/ai/ML-Tagger/tech", function(req, res) {
+	res.sendFile(path.join(__dirname, "/public/html/ai/ML-Tagger/tech.html"));
+});
 
-	// Loop through lines of text file and put in array
-	const stuff = [];
-	fs.readFile("stuff.txt", "utf8", function(err, data) {
-		if (err) console.log(err);
-		
-		const splitData = data.split("\r\n");
-	
-		for (let i = 0; i < splitData.length; i++) {
-			stuff.push(splitData[i]);
-		}
-
-		const posted = true;
-
-		res.render(path.join(__dirname, "/views/secretstuff.ejs"), {posted: posted, stuff: stuff});
-	});
+app.get("/ai/ML-Tagger/sussy", function(req, res) {
+	res.sendFile(path.join(__dirname, "/public/html/ai/ML-Tagger/sussy.html"));
 });
 
 app.get('/css/main.css', function(req, res) {
